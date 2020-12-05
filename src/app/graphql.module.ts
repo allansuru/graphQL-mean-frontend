6fec08d89be6eeea53d84418108d58463c1db4f7
SuRu <allansuru@hotmail.com>
2020-12-05 16:38:24 -0300

    ajustando env

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
+import { environment } from '@env/environment';
 
-const uri = 'http://localhost:3000/graphql'; // <-- add the URL of the GraphQL server here
+const uri = environment.baseUrl; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
