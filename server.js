3f9cd0df384bdc6bcb67c7e66651897e8a250caf
SuRu <allansuru@hotmail.com>
2020-12-06 15:35:44 -0300

    q cu

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/frontend'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(`./frontend/dist/index.html`);
-    //res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
 });
 
 console.log('Console listening');
